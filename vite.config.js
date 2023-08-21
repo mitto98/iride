d781f3d979f4c7c205b9f68ca72c04a9a0c45333
Mattia Dapino <mattia@dapino.dev>
2023-08-21 21:10:40 +0200

    feat: build process generate ts types

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,22 @@ module.exports = {
   build: {
     sourcemap: true,
     emptyOutDir: false,
+    // Reduce bloat from legacy polyfills.
+    target: "esnext",
+    // Leave minification up to applications.
+    minify: false,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: name,
     },
     rollupOptions: {
-      plugins: [typescript()],
+      plugins: [
+        typescript({
+          rootDir: path.resolve(__dirname, "src"),
+          declaration: true,
+          declarationDir: path.resolve(__dirname, "dist"),
+        }),
+      ],
     },
   },
 };
